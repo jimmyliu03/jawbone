38c310dd745f4f9498a442a4565b1fe2d5c576c1
Jimmy Liu <jimmyliu03@gmail.com>
2014-08-13 00:43:23 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jawbone::Application.config.secret_key_base = '8f08cee9ace37e70d66a58a1f93d6b46a02fc6a1a09e9aa6c1b9321406f4b86207e98edce4ebffbc36d2f2fec29c9078c2a5760d12f276c223e665aa32dfad07'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jawbone::Application.config.session_store :cookie_store, key: '_jawbone_session'
