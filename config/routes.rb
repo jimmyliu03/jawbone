8051a719b6f952768989ab5b2730e05698456a73
Jimmy Liu <jimmyliu03@gmail.com>
2014-08-14 21:36:35 -0700

    take 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Jawbone::Application.routes.draw do
 
 
 get   '/login', :to => 'sessions#new', :as => :login
-match '/auth/:provider/callback', :to => 'sessions#create', via: [:get, :post]
-match '/auth/failure', :to => 'sessions#failure', via: [:get, :post]
+match '/auth/:provider/callback', :to => 'sessions#create', via: [:get]
+match '/auth/failure', :to => 'sessions#failure', via: [:get]
 
 
 
